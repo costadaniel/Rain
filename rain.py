10af72642253a3bfa1dc2c9cd35579d813a1cb25
costadaniel <dancosta0601@gmail.com>
2018-12-30 00:21:15 -0300

    Começando...

---
+++ rain.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from random import randint
+from time import sleep
+from sys import stdout
+from os import system
+
+floor_charset = [',', '.', '_']
+rain_charset = ['o', 'O', '(', ')']
+output_string = ""
+
