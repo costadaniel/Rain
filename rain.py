a80add3a3c760001b65e92c840df91e83983aa36
costadaniel <dancosta0601@gmail.com>
2019-01-11 19:45:06 -0300

    Initial floor printing

--- rain.py
+++ rain.py
@@ -9,3 +9,24 @@ floor_charset = [',', '.', '_']
 rain_charset = ['o', 'O', '(', ')']
 output_string = ""
 
+system("clear")
+
+# Printing the floor
+for y in range (0, 24):
+    stdout.write(" " * 80)
+    stdout.flush()
+    stdout.write("\b"*81)
+
+    for x in range (0, 80):
+        sleep(0.01)
+        character = floor_charset[randint(0,2)]
+        output_string += character
+        stdout.write(character)
+        stdout.flush()
+    
+    if(y != 23):
+        stdout.write("\n")
+    else:
+        variable = input()
+
+# Print the word: Rain
